d031edec7a2817b03ef91a69ece19ef42c3a8bcb
Paweł Piorun <dev.pawelpiorun@gmail.com>
2021-04-03 17:49:42 +0200

    Add components to Spark.Core.Client

---
+++ Spark.Core.Client/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Spark.Core.Client.Auth;
+using Spark.Core.Client.Services;
+
+namespace Spark.Core.Client.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static void AddSparkCoreClient(this IServiceCollection services)
+        {
+            services.AddScoped<IHttpService, HttpService>();
+            services.AddScoped<ILocalStorageService, LocalStorageService>();
+
+            services.AddAuthorizationCore();
+
+            // same instance of auth provider and login service
+            services.AddScoped<JwtAuthenticationStateProvider>();
+            services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>(
+                provider => provider.GetRequiredService<JwtAuthenticationStateProvider>());
+
+            services.AddScoped<ILoginService, JwtAuthenticationStateProvider>(
+                provider => provider.GetRequiredService<JwtAuthenticationStateProvider>());
+        }
+    }
+}
